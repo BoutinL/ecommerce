b985c2095f2a1e741ee3af4642e009b3488d406c
unknown <boutin.loic@hotmail.com>
2022-09-12 09:56:05 +0200

    Reverting to procédure 1 try 2>

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -2,15 +2,14 @@
 
 namespace App\Entity;
 
-use DateTimeImmutable;
-use Doctrine\ORM\Mapping as ORM;
-use App\Repository\UsersRepository;
 use App\Entity\Trait\CreatedAtTrait;
-use Doctrine\Common\Collections\Collection;
+use App\Repository\UsersRepository;
 use Doctrine\Common\Collections\ArrayCollection;
-use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UsersRepository::class)]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
@@ -61,7 +60,7 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
     public function __construct()
     {
         $this->orders = new ArrayCollection();
-        $this->created_at = new DateTimeImmutable();
+        $this->created_at = new \DateTimeImmutable();
     }
 
     public function getId(): ?int
